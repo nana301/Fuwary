1c1db73643283354cc3b532fb7c2e12615fca336
nanap <iti_hana_rin@yahoo.co.jp>
2026-01-29 19:04:01 +0900

    readme追加　差別化・シミュレーション

--- db/migrate/20260128085120_remove_tarot_result_from_tarot_cards.rb
+++ db/migrate/20260128085120_remove_tarot_result_from_tarot_cards.rb
@@ -1,5 +1,5 @@
-class RemoveTarotResultFromTarotCards < ActiveRecord::Migration[7.2]
+class RemoveTarotResultFromTarotCards < ActiveRecord::Migration[7.0]
   def change
-    remove_reference :tarot_cards, :tarot_result, null: false, foreign_key: true
+    remove_reference :tarot_cards, :tarot_result, foreign_key: true
   end
 end
