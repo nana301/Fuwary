aef0714f4bf073afb6b914087ac07261457bb766
nanap <iti_hana_rin@yahoo.co.jp>
2026-02-05 13:50:02 +0900

    マイグレーションエラー修正

--- db/migrate/20260205030158_add_generated_on_and_mode_to_tarot_results.rb
+++ db/migrate/20260205030158_add_generated_on_and_mode_to_tarot_results.rb
@@ -2,8 +2,8 @@ class AddGeneratedOnAndModeToTarotResults < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
 
   def up
-    add_column :tarot_results, :mode, :string
-    add_column :tarot_results, :generated_on, :date
+    add_column :tarot_results, :mode, :string unless column_exists?(:tarot_results, :mode)
+    add_column :tarot_results, :generated_on, :date unless column_exists?(:tarot_results, :generated_on)
 
     execute <<~SQL
       UPDATE tarot_results
@@ -14,6 +14,16 @@ class AddGeneratedOnAndModeToTarotResults < ActiveRecord::Migration[7.2]
     change_column_null :tarot_results, :mode, false
     change_column_null :tarot_results, :generated_on, false
 
+    -- ★ ここが重要：重複削除
+    execute <<~SQL
+      DELETE FROM tarot_results a
+      USING tarot_results b
+      WHERE a.id > b.id
+        AND a.user_id = b.user_id
+        AND a.mode = b.mode
+        AND a.generated_on = b.generated_on
+    SQL
+
     add_index :tarot_results, [:user_id, :mode, :generated_on],
               unique: true,
               name: "index_tarot_results_unique_daily_per_mode",
@@ -22,7 +32,7 @@ class AddGeneratedOnAndModeToTarotResults < ActiveRecord::Migration[7.2]
 
   def down
     remove_index :tarot_results, name: "index_tarot_results_unique_daily_per_mode"
-    remove_column :tarot_results, :generated_on
-    remove_column :tarot_results, :mode
+    remove_column :tarot_results, :generated_on if column_exists?(:tarot_results, :generated_on)
+    remove_column :tarot_results, :mode if column_exists?(:tarot_results, :mode)
   end
 end
